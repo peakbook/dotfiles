a517e56b699057fb51d5c02a2436f52f8e189b58
peakbook <peakbook@users.noreply.github.com>
2025-04-22 22:48:56 +0900

    refactor(config): update nvim-config-local settings

--- .config/nvim/lua/rc/config/nvim-config-local.lua
+++ .config/nvim/lua/rc/config/nvim-config-local.lua
@@ -1,9 +1,8 @@
-require("config-local").setup {
-  -- Default configuration (optional)
-  config_files = { ".vimrc.lua", ".vimrc" },            -- Config file patterns to load (lua supported)
-  hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
-  autocommands_create = true,                           -- Create autocommands (VimEnter, DirectoryChanged)
-  commands_create = true,                               -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
-  silent = false,                                       -- Disable plugin messages (Config loaded/ignored)
-  lookup_parents = false,                               -- Lookup config files in parent directories
-}
+require("config-local").setup({
+	config_files = { ".nvim.lua", ".nvimrc" },
+	hashfile = vim.fn.stdpath("data") .. "/config-local",
+	autocommands_create = true,
+	commands_create = true,
+	silent = false,
+	lookup_parents = false,
+})
